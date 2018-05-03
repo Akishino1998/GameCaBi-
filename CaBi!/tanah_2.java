2c9b2dbd20da3b7097e6c3e089d03253ba07e9a3
Akishino1998 <ekopujianto48@gmail.com>
2018-05-03 17:49:45 +0800

    tanah gerak

--- CaBi!/tanah_2.java
+++ CaBi!/tanah_2.java
@@ -1,11 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class tanah_2 here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class tanah_2 extends Actor
 {
     /**
